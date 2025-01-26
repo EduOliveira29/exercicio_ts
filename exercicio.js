f903a0163753b8652591983a44f35c5cf34cdcf1
EduOliveira29 <144156214+EduOliveira29@users.noreply.github.com>
2025-01-26 13:01:52 -0300

    Update exercicio.js
    correção na função saudação

--- exercicio.js
+++ exercicio.js
@@ -4,7 +4,7 @@ function multiplicaNumeros(multiplicadorA, multiplicadorB) {
     return multiplicadorA * multiplicadorB;
 }
 function saudação(nome) {
-    console.log('Ola ' + nome);
+    return "Ola " + nome;
 }
 saudação(nome);
 multiplicaNumeros(2, 3);
